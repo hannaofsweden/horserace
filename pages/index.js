178701eab2200b06200f6ea6648d3d4360f9154d
johanna.johansson <johanna.johansson@bonniernews.se>
2021-06-04 15:12:17 +0200

    Update styling

--- pages/index.js
+++ pages/index.js
@@ -1,8 +1,9 @@
-const styles = require('../styles/Home.module.less');
 import { Radio, Typography, Row, Descriptions, Table, Divider } from 'antd';
 const { Title } = Typography;
 import { useState, useEffect } from 'react';
 import axios from 'axios';
+import Head from 'next/head';
+const styles = require('../styles/Home.module.less');
 
 const getRace = async (race) => {
 	const response = await axios.get(`https://www.atg.se/services/racinginfo/v1/api/games/${race.id}`);
@@ -37,11 +38,19 @@ const columns = [
 	},
 ];
 
+const expandedRowRender = (record) => (
+	<p style={{ margin: 0 }}>
+		<b>Trainer: </b>
+		{record.horse.trainer?.firstName} {record.horse?.trainer.lastName} <b>Horse father: </b>
+		{record.horse?.pedigree?.father?.name}
+	</p>
+);
+
 const Home = () => {
 	const [gameType, setGameType] = useState('V75');
 	const [currentGame, setCurrentGame] = useState({ id: 'Loading...', races: [] });
 
-	const onChange = ({ target: { value } }) => {
+	const handleGameTypeChange = ({ target: { value } }) => {
 		setGameType(value);
 	};
 
@@ -59,56 +68,57 @@ const Home = () => {
 
 	return (
 		<div>
-			<Row>
-				<Radio.Group onChange={onChange} defaultValue={gameType} buttonStyle="solid">
-					<Radio.Button value="V75">V75</Radio.Button>
-					<Radio.Button value="V65">V65</Radio.Button>
-					<Radio.Button value="V64">V64</Radio.Button>
-					<Radio.Button value="V4">V4</Radio.Button>
-				</Radio.Group>
-			</Row>
-			<Row>
-				<Title level={2}>{gameType}</Title>
-			</Row>
-			<Row>
-				<Title level={3}>Races:</Title>
-			</Row>
-			{currentGame.races.map((race, index) => (
-				<div className={styles.raceDescription} key={race.id}>
-					<Row>
-						<Title level={4}>{race.name || race.track?.name}</Title>
-					</Row>
-					<Row className={styles.raceDescription}>
-						<Descriptions bordered>
-							<Descriptions.Item label="Race number">{race.number}</Descriptions.Item>
-							<Descriptions.Item label="Start time">{formatTime(race.startTime)}</Descriptions.Item>
-						</Descriptions>
-					</Row>
-					<Row>
-						<Table
-							bordered
-							style={{ width: '100%' }}
-							columns={columns}
-							dataSource={race.starts}
-							pagination={false}
-							rowKey="number"
-							expandable={{
-								expandedRowRender: (record) => (
-									<p style={{ margin: 0 }}>
-										<b>Trainer: </b>
-										{record.horse.trainer.firstName} {record.horse.trainer.lastName}{' '}
-										<b>Horse father: </b>
-										{record.horse.pedigree.father.name}
-									</p>
-								),
-							}}
-						/>
-					</Row>
-					<Row>
-						<Divider />
-					</Row>
-				</div>
-			))}
+			<Head>
+				<title>ATG {gameType}</title>
+				<link rel="icon" href="favicon.png" />
+			</Head>
+			<div className={styles.container}>
+				<Row>
+					<Radio.Group
+						onChange={handleGameTypeChange}
+						defaultValue={gameType}
+						buttonStyle="solid"
+						size="large"
+					>
+						<Radio.Button value="V75">V75</Radio.Button>
+						<Radio.Button value="V65">V65</Radio.Button>
+						<Radio.Button value="V64">V64</Radio.Button>
+						<Radio.Button value="V4">V4</Radio.Button>
+					</Radio.Group>
+					<Divider />
+				</Row>
+				{currentGame.races.map((race, index) => (
+					<div className={styles.raceDescription} key={race.id}>
+						<Row>
+							<Title level={4}>
+								{gameType}-{race.number}: {race.name || race.track?.name}
+							</Title>
+						</Row>
+						<Row className={styles.raceDescription}>
+							<Descriptions bordered>
+								<Descriptions.Item label="Race number">{race.number}</Descriptions.Item>
+								<Descriptions.Item label="Start time">{formatTime(race.startTime)}</Descriptions.Item>
+							</Descriptions>
+						</Row>
+						<Row>
+							<Table
+								bordered
+								style={{ width: '100%' }}
+								columns={columns}
+								dataSource={race.starts}
+								pagination={false}
+								rowKey="number"
+								expandable={{
+									expandedRowRender,
+								}}
+							/>
+						</Row>
+						<Row>
+							<Divider />
+						</Row>
+					</div>
+				))}
+			</div>
 		</div>
 	);
 };

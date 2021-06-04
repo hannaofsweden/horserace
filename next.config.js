e92977a20c5b1630dd0cd1cbf01bc49330fa3dc0
johanna.johansson <johanna.johansson@bonniernews.se>
2021-06-04 09:57:55 +0200

    Initial setup

---
+++ next.config.js
@@ -0,0 +1,17 @@
+const withAntdLess = require('next-plugin-antd-less');
+
+module.exports = withAntdLess({
+	modifyVars: {},
+	lessVarsFilePath: './src/styles/variables.less',
+	lessVarsFilePathAppendToEndOfContent: false,
+	cssLoaderOptions: {},
+
+	webpack(config) {
+		return config;
+	},
+
+	future: {
+		// if you use webpack5
+		webpack5: true,
+	},
+});
--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,8 @@
-import '../styles/globals.css'
+require('../styles/globals.less');
+require('antd/dist/antd.less');
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+	return <Component {...pageProps} />;
 }
 
-export default MyApp
+export default MyApp;
